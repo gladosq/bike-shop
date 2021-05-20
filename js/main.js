1326c7814de1caf1c02befb1cc09700a61a1fb1b
gladosq <69731271+gladosq@users.noreply.github.com>
2021-05-20 11:38:53 +0000

    Deploying to gh-pages from @ gladosq/bike-shop@cd90062598bed6ca52b8f56f1b838a7ea0c52849 🚀

--- js/main.js
+++ js/main.js
@@ -6,10 +6,15 @@ var headerNavigation = document.querySelector('.header__nav');
 var headerNavItem = document.querySelectorAll('.header__nav-item');
 menuButtonClosed.addEventListener('click', function () {
   headerNavigation.classList.toggle('header__nav--opened');
+  document.body.classList.toggle('no-scroll');
 });
 menuButtonOpened.addEventListener('click', function () {
   if (headerNavigation.classList.contains('header__nav--opened')) {
     headerNavigation.classList.remove('header__nav--opened');
+  }
+
+  if (document.body.classList.contains('no-scroll')) {
+    document.body.classList.remove('no-scroll');
   }
 });
 headerNavItem.forEach(function (item) {
@@ -17,7 +22,20 @@ headerNavItem.forEach(function (item) {
     if (headerNavigation.classList.contains('header__nav--opened')) {
       headerNavigation.classList.remove('header__nav--opened');
     }
+
+    if (document.body.classList.contains('no-scroll')) {
+      document.body.classList.remove('no-scroll');
+    }
   });
+});
+window.addEventListener('resize', function () {
+  if (headerNavigation.classList.contains('header__nav--opened')) {
+    headerNavigation.classList.remove('header__nav--opened');
+  }
+
+  if (document.body.classList.contains('no-scroll')) {
+    document.body.classList.remove('no-scroll');
+  }
 });
 "use strict";
 
